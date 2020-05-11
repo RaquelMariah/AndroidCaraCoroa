13c58d8c739e4a35669e4ba53ff9f85b19806aa4
Raquel Maria <raquel.maria@vr.com.br>
2020-05-11 18:00:24 -0300

    Adicionada identificação p/ botões + listener botão jogar

--- app/src/main/java/com/ramaria/androidcaracoroa/MainActivity.java
+++ app/src/main/java/com/ramaria/androidcaracoroa/MainActivity.java
@@ -2,13 +2,29 @@ package com.ramaria.androidcaracoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button buttonJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonJogar = findViewById(R.id.buttonJogar);
+
+        buttonJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
