48bafbc6bb3bfd90021d96a6412c6d69519a574e
Raquel Maria <raquel.maria@vr.com.br>
2020-05-11 21:27:17 -0300

    Adicionado listener botão voltar

--- app/src/main/java/com/ramaria/androidcaracoroa/ResultActivity.java
+++ app/src/main/java/com/ramaria/androidcaracoroa/ResultActivity.java
@@ -3,25 +3,36 @@ package com.ramaria.androidcaracoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ResultActivity extends AppCompatActivity {
 
     private ImageView imageResult;
+    private Button buttonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         imageResult = findViewById(R.id.imageResult);
-
+        buttonVoltar = findViewById(R.id.buttonVoltar);
         Bundle dados = getIntent().getExtras();
         int numero = dados.getInt("numero");
 
-        if(numero == 0) {
-            imageResult.setImageResource(R.drawable.moeda_cara);
-        }else {
-            imageResult.setImageResource(R.drawable.moeda_coroa);
-        }
+            if(numero == 0) {
+                imageResult.setImageResource(R.drawable.moeda_cara);
+            }else {
+                imageResult.setImageResource(R.drawable.moeda_coroa);
+            }
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 }
